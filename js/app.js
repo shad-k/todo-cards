109c73631c1207a904e0c061f0fb677584983021
shad-k <shadab.2005@gmail.com>
2017-06-27 19:52:41 +0530

    Add modal for adding new todo

--- js/app.js
+++ js/app.js
@@ -1,15 +1,17 @@
 $(function() {
+	// Contains all methods related to Data of the App
 	var Data = {
 		init: function() {
 			if(typeof (Storage) !== "undefined") {
 
 			} else {
 				var todoJSON = localStorage.getItem("todos") || {};
+				console.log(todoJSON);
 			}
 		},
 	};
 
-
+	// The main controller of the app
 	var App = {
 		init: function() {
 			Data.init();
@@ -17,10 +19,34 @@ $(function() {
 		},
 	};
 
-
+	// This handles all the view related things
 	var View = {
 		init: function() {
+			// Add all bindings
 			this.addBindings();
+		},
+		addBindings: function() {
+			// Displays the modal
+			$(".addButton").click(function() {
+				$(".todoModal").css("display", "flex");
+			});
+
+			// Hides the modal
+			$(".closeModal").click(function() {
+				$(".todoModal").hide();
+			});
+
+			// Adds a new todo
+			$(".submit").click(function() {
+				View.addNewTodo($(".inputText").val());
+				$(".todoModal").hide();
+			});
+		},
+		addNewTodo: function(todo) {
+			console.log(todo);
 		}
 	}
+
+
+	App.init();
 });

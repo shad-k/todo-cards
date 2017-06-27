79980b4dbb1c91d8013af33112ce6bc3fe9c8d67
shad-k <shadab.2005@gmail.com>
2017-06-27 19:16:15 +0530

    Add init methods in JS

--- js/app.js
+++ js/app.js
@@ -1,3 +1,26 @@
 $(function() {
+	var Data = {
+		init: function() {
+			if(typeof (Storage) !== "undefined") {
 
+			} else {
+				var todoJSON = localStorage.getItem("todos") || {};
+			}
+		},
+	};
+
+
+	var App = {
+		init: function() {
+			Data.init();
+			View.init();
+		},
+	};
+
+
+	var View = {
+		init: function() {
+			this.addBindings();
+		}
+	}
 });

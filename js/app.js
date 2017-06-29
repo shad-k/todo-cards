1141d56dbd897d8e9ab61522073a9a5ace1388d7
shad-k <shadab.2005@gmail.com>
2017-06-29 20:40:33 +0530

    Add code to restore todos from local storage

--- js/app.js
+++ js/app.js
@@ -1,22 +1,35 @@
 
 // Contains all methods related to Data of the App
 var Data = {
+	todos: [],
 	init: function() {
 		if(typeof (Storage) !== "undefined") {
-
+			this.todos = JSON.parse(localStorage.getItem("todos")) || [];
 		} else {
-			var todoJSON = localStorage.getItem("todos") || {};
-			console.log(todoJSON);
+			this.todos = null;
 		}
+		return this.todos;
 	},
+	addTodo: function(todo) {
+		this.todos.push(todo);
+	}
 };
 
 // The main controller of the app
 var App = {
 	init: function() {
-		Data.init();
-		View.init();
+		window.onload = function() {
+			var todos = Data.init();
+			View.init(todos);
+		}
+
+		window.onclose = function() {
+			// Save current todos list
+		}
 	},
+	addTodoToData: function(todo) {
+		Data.addTodoToData(todo);
+	}
 };
 
 // This handles all the view related things
@@ -24,9 +37,12 @@ var View = {
 	addButton: document.querySelector(".addButton"),
 	todoModal: document.querySelector(".todoModal"),
 	cards: document.querySelector(".cards"),
-	init: function() {
+	init: function(todos) {
 		// Add all bindings
 		this.addBindings();
+
+		// Populate todos from local storage
+		this.addTodos(todos);
 	},
 	addBindings: function() {
 		// Displays the modal
@@ -44,20 +60,37 @@ var View = {
 		var submit = document.querySelector(".submit");
 		submit.addEventListener("click", function() {
 			var inputText = document.querySelector(".inputText");
-			View.addNewTodo(inputText.value);
+
+			// Get the current date
+			var dateObj = new Date();
+
+			// Convert to a readable format in the current locale
+			var date = dateObj.toLocaleDateString(language);
+			var todo = 	{
+							text: inputText.value,
+							date: date
+						};
+			View.addNewTodo(todo);
 			View.todoModal.style.display = "none";
 			inputText.value = "";
+
+			App.addTodoToData(todo);
 		});
 	},
+	addTodos: function(todos) {
+		if(todos) {
+			for(var i = 0; i < todos.length; i++) {
+				this.addNewTodo(todos[i]);
+			}
+		} else {
+			alert("Your browser does not support local storage." +
+				" Please use a browser that support local storage to enjoy the app!");
+		}
+	},
 	addNewTodo: function(todo) {
+		console.log(todo);
 		// Get the user locale
 		var language = (navigator.language || navigator.browserLanguage);
-
-		// Get the current date
-		var dateObj = new Date();
-
-		// Convert to a readable format in the current locale
-		var date = dateObj.toLocaleDateString(language);
 
 		var html = '<div class="card"> ' +
 						'<div class="actionDiv">' +
@@ -72,14 +105,14 @@ var View = {
 							'</div>' +
 						'</div>' +
 						'<div class="textDiv">' +
-							'<span class="text">' + todo + '</span>' +
+							'<span class="text">' + todo.text + '</span>' +
 						'</div>' +
 						'<div class="dateDiv">' +
-							'<span class="date">' + date + '</span>' +
+							'<span class="date">' + todo.date + '</span>' +
 						'</div>' +
 					'</div>';
 
-		this.cards.innerHTML = html;
+		this.cards.innerHTML += html;
 	}
 }
 
